f43d49de38c798d90994b239b992d4676ac14309
R1ku2 <riku8798@gmail.com>
2019-06-05 10:09:18 +0900

    Inplements main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <stdio.h>
-int main(int argc, int** argv)
+#include <stdlib.h>
+int main(int argc, char **argv)
 {
-    
+    if(argc != 2)
+    {
+        fprintf(stderr, "Invalid args.\n");
+        return 1;
+    }
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf(" mov rax, %d\n",atoi(argv[1]));
+    printf(" ret\n");
+    return 0;
 }
